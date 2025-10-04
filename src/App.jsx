e995cf013394d3e2b54315c15ee72eeece00f3c3
nazmulhoque69 <nazmulhoque1530@gmail.com>
2025-10-04 08:08:46 +0600

    Button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import List from './component/List'
 import Home from './component/Home'
 import Stat from './component/Stat'
 import Footer from './component/Footer'
+import Button from './component/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,8 @@ function App() {
   return (
     <>
     <Home/>
-      
+    <Button/>
+      <div className='  mx-auto '>
 <h1>Hello I am Nazmul!</h1>
 {/* Open the modal using document.getElementById('ID').showModal() method */}
 <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
@@ -29,6 +31,7 @@ function App() {
     </div>
   </div>
 </dialog>
+</div>
 <List/>
 <Stat/>
 <Footer/>
---
+++ src/component/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>Button
+        <div className='flex justify-evenly'>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+
+
+</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Button
